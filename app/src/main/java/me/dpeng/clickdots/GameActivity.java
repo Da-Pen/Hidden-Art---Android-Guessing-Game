add90727dc49b49a1b7be52329809e19ba178b61
Daniel Peng <peng.daniel11440@gmail.com>
2018-07-20 12:08:22 -0400

    completed "Build your first app" tutorial

--- app/src/main/java/me/dpeng/clickdots/GameActivity.java
+++ app/src/main/java/me/dpeng/clickdots/GameActivity.java
@@ -1,7 +1,9 @@
 package me.dpeng.clickdots;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // get the intent from the last activity
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MenuActivity.EXTRA_MESSAGE);
+
+        // set the textview in this activity to the message
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
